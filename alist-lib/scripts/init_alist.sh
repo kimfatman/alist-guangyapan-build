722a985c8fe3154fc5063b05e3dee9315761587c
kimfatman <237029918@QQ.com>
2026-05-18 17:05:01 +0800

    Add init_alist.sh (clones AlistGo/alist with guangyapan driver)

---
+++ alist-lib/scripts/init_alist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+GIT_REPO="https://github.com/AlistGo/alist.git"
+echo "Using AList repo: $GIT_REPO"
+
+TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
+echo "AList - ${TAG_NAME}"
+
+rm -rf ./src
+unset GIT_WORK_TREE
+git clone --branch "$TAG_NAME" $GIT_REPO ./src
+rm -rf ./src/.git
+mv -f ./src/* ../
+rm -rf ./src
+cd ../
+go mod edit -replace github.com/djherbis/times@v1.6.0=github.com/jing332/times@latest
